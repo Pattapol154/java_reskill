f646e84799bd30de24804aaa3bc8c54e2af7cafc
Pattapol tankanpai <57236797+Pattapol154@users.noreply.github.com>
2024-04-26 18:27:52 +0700

    Update java_oop_day1.java

--- concept_oop.java
+++ concept_oop.java
@@ -1,11 +1,19 @@
 //ตัวอย่างการสร้าง Class เเละ Field / Attribute
 
+//Employee.java
 public class Employee {													
-  private String id; --Attribute  
-	private String name; --Attribute
-	private Double salary; --Attribute
+  private String id; //Attribute  
+	private String name; //Attribute
+	private Double salary; //Attribute
 }
 
 //private,public = modifier
 //String,double = data type
 //id,name,salary = name
+
+//Main.java
+public class Main{
+	public static void main(String [] args){
+		Employee e1=new Employee(); //create object
+	}
+}
