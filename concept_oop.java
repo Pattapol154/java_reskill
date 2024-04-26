d6234d4dafa2f0ea54c1ebc814482e2ee46503b3
Pattapol tankanpai <57236797+Pattapol154@users.noreply.github.com>
2024-04-26 22:59:58 +0700

    Update concept_oop.java

--- concept_oop.java
+++ concept_oop.java
@@ -4,7 +4,23 @@
 public class Employee {	
   private String id; //Attribute  
   private String name; //Attribute
-	private Double salary; //Attribute
+  private Double salary; //Attribute
+
+	
+	//Create method
+	public void setId(String id) {
+		this.id=id; //key word [this] การใช้คีย์เวิร์ด this คือ เมื่อต้องการเรียกใช้ constructor อื่นๆ ที่อยู่ภายใน class เดียวกัน
+	}
+
+	//Create method
+	public void setName(String name) {
+		this.name=name; //key word [this] การใช้คีย์เวิร์ด this คือ เมื่อต้องการเรียกใช้ constructor อื่นๆ ที่อยู่ภายใน class เดียวกัน
+	}
+
+	//Create method
+	public void setSalary(String salary) {
+		this.salary=salary; //key word [this] การใช้คีย์เวิร์ด this คือ เมื่อต้องการเรียกใช้ constructor อื่นๆ ที่อยู่ภายใน class เดียวกัน
+	}
 }
 
 //private,public = modifier
@@ -15,5 +31,10 @@ public class Employee {
 public class Main{
 	public static void main(String [] args){
 		Employee e1=new Employee(); //create object
+
+		e1.setID("1"); //assign method
+		e1.setName("test1"); //assign method
+		e1.setSalary(30000.0); //assign method
+		
 	}
 }
