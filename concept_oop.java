184f1283ebcbaa6b42903a046c53b0fbf5922f5b
Pattapol tankanpai <57236797+Pattapol154@users.noreply.github.com>
2024-04-26 23:45:04 +0700

    Update oop_java_concept.java

--- concept_oop.java
+++ concept_oop.java
@@ -6,7 +6,6 @@ public class Employee {
   private String name; //Attribute
   private Double salary; //Attribute
 
-	
 	//Create method
 	public void setId(String id) {
 		this.id=id; //key word [this] การใช้คีย์เวิร์ด this คือ เมื่อต้องการเรียกใช้ constructor อื่นๆ ที่อยู่ภายใน class เดียวกัน
@@ -21,6 +20,23 @@ public class Employee {
 	public void setSalary(String salary) {
 		this.salary=salary; //key word [this] การใช้คีย์เวิร์ด this คือ เมื่อต้องการเรียกใช้ constructor อื่นๆ ที่อยู่ภายใน class เดียวกัน
 	}
+
+	//Create method
+	public void displayEmployee(){
+		System.out.println("ID ="+this.id); //key word [this] การใช้คีย์เวิร์ด this คือ เมื่อต้องการเรียกใช้ constructor อื่นๆ ที่อยู่ภายใน class เดียวกัน
+		System.out.println("Name ="+this.name); //key word [this] การใช้คีย์เวิร์ด this คือ เมื่อต้องการเรียกใช้ constructor อื่นๆ ที่อยู่ภายใน class เดียวกัน
+		System.out.println("Salary ="+this.salary); //key word [this] การใช้คีย์เวิร์ด this คือ เมื่อต้องการเรียกใช้ constructor อื่นๆ ที่อยู่ภายใน class เดียวกัน
+	}
+
+	//Create method
+	public String getName(){
+		return this.name;
+	}
+
+	//Create method
+	public String getSalary(){
+		return this.salary;
+	}
 }
 
 //private,public = modifier
@@ -32,9 +48,12 @@ public class Main{
 	public static void main(String [] args){
 		Employee e1=new Employee(); //create object
 
-		e1.setID("1"); //assign method
-		e1.setName("test1"); //assign method
-		e1.setSalary(30000.0); //assign method
-		
+		e1.setID("1"); //assign method set
+		e1.setName("test1"); //assign method set
+		e1.setSalary(30000.0); //assign method set
+		e1.displayEmployee(); //assign method set
+
+		System.out.println(""+e1.getName()); //assign method get
+		System.out.println(""+e1.getSalary()); //assign method get
 	}
 }
