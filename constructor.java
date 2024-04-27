2fa63cb0c1deb7587c65098da53942018f25545c
Pattapol tankanpai <57236797+Pattapol154@users.noreply.github.com>
2024-04-27 10:58:28 +0700

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -1,21 +1,32 @@
 //Employee.java
+public class Employee {	
+  private String id; //Attribute  
+  private String name; //Attribute
+  private Double salary; //Attribute
+  
+  //Default Constructor
+  public Employee(){
+  
+  }
+  //Custom Constructor
+  public Employee(String id,String name){
 
-//Default Constructor
-public Employee(){
-
-}
-
-
-
-
-
-
-
-//Main.java
-public class Main{
-  public static void main(String [] args){
-
-    Employee e1=new Employee(); //create object
+  }
+  //Custom Constructor
+  public Employee(String id,String name,Double salary){
+    this.id=id;
+    this.name=name;
+    this.salary=salary;
   }
 
+  //Main.java
+  public class Main{
+    public static void main(String [] args){
+  
+      Employee e1=new Employee("1","test1",30000.0); //create object Default Constructor
+      Employee e2=new Employee(); //create object Custom Constructor
+      Employee e1=new Employee("3","test3"); //create object Custom Constructor
+    }
+  
+  }
 }
