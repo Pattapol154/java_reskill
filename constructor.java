153ab99aca2ad45782e40b728b200d5fd5023fe6
Pattapol tankanpai <57236797+Pattapol154@users.noreply.github.com>
2024-04-27 10:20:22 +0700

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -1,1 +1,21 @@
+//Employee.java
 
+//Default Constructor
+public Employee(){
+
+}
+
+
+
+
+
+
+
+//Main.java
+public class Main{
+  public static void main(String [] args){
+
+    Employee e1=new Employee(); //create object
+  }
+
+}
